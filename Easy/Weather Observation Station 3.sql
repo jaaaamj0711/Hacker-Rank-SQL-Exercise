3c2aa8ff90f4c8974b84181053f81bef87a5e274
seominji <55734436+jaaaamj0711@users.noreply.github.com>
2021-10-20 18:46:38 +0900

    Update and rename Weather Observation Station 3.sql to Easy/Weather Observation Station 3.sql

---
+++ Easy/Weather Observation Station 3.sql
@@ -0,0 +1,5 @@
+SELECT 
+    DISTINCT city
+FROM station
+WHERE
+    id%2 = 0
--- Weather Observation Station 3.sql
+++
@@ -1,9 +0,0 @@
-/* mysql */
-SELCT distinct city 
-FROM station
-WHERE id%2=0;
-
-/* oracle */
-SELECT DISTINCT CITY
-FROM STATION
-WHERE MOD(ID, 2) = 0;
