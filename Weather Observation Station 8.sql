831bd9fb015f563f803e67dd3a09bfec87b57bb0
seominji <55734436+jaaaamj0711@users.noreply.github.com>
2021-10-21 09:56:53 +0900

    Update Weather Observation Station 8.sql

--- Weather Observation Station 8.sql
+++ Weather Observation Station 8.sql
@@ -1,4 +1,17 @@
 /*
 문자열 함수 활용
 */
-SELECT DISTINCT(CITY) FROM STATION WHERE left(city,1) in ('a','i','e','o','u') and right(city,1) in ('a','i','e','o','u');
+SELECT DISTINCT city 
+  FROM station 
+WHERE 
+  left(city,1) in ('a','i','e','o','u') AND right(city,1) in ('a','i','e','o','u')
+
+
+/*
+정규 표현식 활용
+*/
+SELECT 
+    DISTINCT city
+FROM station
+WHERE 
+    city REGEXP '[aeiou]$' AND city REGEXP '^[aeiou]'
