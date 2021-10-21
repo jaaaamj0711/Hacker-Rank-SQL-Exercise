bffccf1846563fcdcf22af910965f9274c4f39af
seominji <55734436+jaaaamj0711@users.noreply.github.com>
2021-10-22 07:19:01 +0900

    Update Weather Observation Station 11.sql

--- Weather Observation Station 11.sql
+++ Weather Observation Station 11.sql
@@ -1,1 +1,19 @@
-SELECT DISTINCT CITY FROM STATION WHERE left(city,1) not in ('a','e','i','o','u') or right(city,1) not in ('a','e','i','o','u');
+/*
+문자열 함수 활용
+*/
+SELECT
+    DISTINCT city
+FROM station
+WHERE
+    LEFT(city,1) not in ('a', 'e', 'i', 'o', 'u')
+    OR RIGHT(city,1) not in ('a', 'e', 'i', 'o', 'u')
+
+
+/*
+정규 표현식 활용
+*/
+SELECT 
+    DISTINCT city
+FROM station
+WHERE 
+    city REGEXP '^[^aeiou]' OR city REGEXP '[^aeiou]$'
