065d6377674c2f7d2a3cfb00836384b61b935b84
seominji <55734436+jaaaamj0711@users.noreply.github.com>
2021-10-22 07:21:21 +0900

    Weather end

---
+++ Basic Select/Weather Observation Station 12.sql
@@ -0,0 +1,5 @@
+SELECT 
+    DISTINCT city
+FROM station
+WHERE 
+    city REGEXP '^[^aeiou]' AND city REGEXP '[^aeiou]$'
--- Weather Observation Station 12.sql
+++
@@ -1,2 +0,0 @@
-SELECT DISTINCT CITY FROM STATION WHERE left(city,1) not in ('a','e','i','o','u')
-    AND right(city,1) not in ('a','e','i','o','u');
